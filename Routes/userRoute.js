b6bb6b6d7f3acf3e7147be61da5a86fafe16602f
Ayellet Tsachi <ayelletts@gmail.com>
2022-07-12 19:43:49 +0300

    signup 2

--- Routes/userRoute.js
+++ Routes/userRoute.js
@@ -31,51 +31,8 @@ router.post("/signup", async (req, res) => {
 
 router.get("/", auth, async (req, res) => {
   try {
-    const user = await userLogic.getUserById(req._id);
-    res.send(user);
-  } catch (error) {
-    console.log(error);
-    res.status(400).send(`Error occured :: ${error.message}`);
-  }
-});
-// router.get("/", (req, res) => {
-//   res.send("Hello World!");
-// });
-
-// router.get("/", async (req, res) => {
-//   const users = await userLogic.getAllUsers();
-//   res.send(users);
-// });
-
-router.get("/new", async (req, res) => {
-  try {
-    const user = await userLogic.getUserById();
-    res.send("ahlan");
-  } catch (error) {
-    console.log(error);
-    res.status(400).send(`Error occured :: ${error.message}`);
-  }
-});
-
-router.post("/", async (req, res) => {
-  const { firstName, lastName, userName } = req.body;
-
-  const restFields = {
-    password: "112233445566",
-    address: {
-      street: "qwerty",
-      homeNum: "55",
-      city: "Eilat",
-    },
-    gender: "female",
-  };
-  const user = { firstName, lastName, userName, ...restFields };
-});
-
-router.put("/edit_user/:id", async (req, res) => {
-  try {
-    const saveUser = await userLogic.updateUser(req.params.id, req.body);
-    res.send(saveUser);
+    const playLists/user = await userLogic.getUserPlaylists(req._id);
+    res.send(playLists/user);
   } catch (error) {
     console.log(error);
     res.status(400).send(`Error occured :: ${error.message}`);
